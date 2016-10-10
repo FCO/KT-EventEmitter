24b7fde49d1518cb9c4741044e25f4d9c8e037df
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2016-10-10 00:51:10 -0300

    Spek

--- test/SimpleTest.kt
+++ test/SimpleTest.kt
@@ -1,7 +1,16 @@
+import com.winterbe.expekt.should
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
 
 /**
  * Created by fernando on 10/10/16.
  */
 class SimpleTest : Spek({
+    println("AQUI")
+    describe("bla") {
+        it("ble") {
+            1.should.be.equal(42)
+        }
+    }
 })
